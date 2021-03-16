a960b53b9ecdc8eb9e1b1b6cef65df5fb2b39480
michaelwang <wangqiangshen@gmail.com>
2021-03-16 20:30:35 +0800

    update demo server

---
+++ readme.md
@@ -0,0 +1,19 @@
+
+# A Simple chat server base on socket.io
+
+
+## Introduction
+A simple server for  [Vue Material Admin](http://vma.isocked.com/#/chat/messaging) as base framework.
+
+
+## Project setup
+```
+npm install
+```
+
+
+### Run server
+```
+node server.js
+
+```
--- server.js
+++ server.js
@@ -1,40 +1,42 @@
 
-const superagent = require('superagent');
+const superagent = require('superagent')
 const http = require('http')
-const server = http.createServer();
+const ROOM = 'VMA'
+const server = http.createServer()
 const io = require('socket.io')(server,{
   cors: {
     origin: "*",
   }
-});
-const  clients = new Set();
+})
+const  clients = new Set()
 let users = [] // online users
 const updateUsers = (user) => {
-  const index = users.findIndex((item) => item.username === user.username)
-  if (index === -1) {
+  const find = users.find((item) => item.clientId === user.clientId)
+  if (find === undefined) {
     users.push(user)
-  } else {
-    users[index] = user
   }
 }
 //handle the socket
 io.sockets.on('connection', (socket) =>  {
-    console.log(socket.id + ': connected')
+    const { address, headers } = socket.handshake
+    const agent = headers['user-agent']
+    const user = { ip: address, clientId: socket.id, agent: agent,username:socket.id, status: 1 }
+    updateUsers(user)
+    console.log(`${socket.id} is using ${agent}  connected from ${address}` )
     clients.add(socket.id)
+    socket.join(ROOM)
     socket.on('join', ( { username, status, clientId } ) => {
-      console.log(username + ': joined')
-      updateUsers({ username, status, clientId })
       io.local.emit('join', users)
     })
     socket.on('message', ( message ) => {
-        console.log(socket.id + `send : ${message.text}`)
-        socket.broadcast.emit('message', message);
-    });
+        console.log(socket.id + `send : ${message.text} ${message.createdAt}`)
+        socket.broadcast.emit('message', message)
+    })
     socket.on('disconnect', () => {
       clients.delete(socket.id)
       users = users.filter(item => item.clientId !== socket.id)
       console.log(socket.id + ': disconnected')
-      socket.broadcast.emit('leave', users);
-  });    
-});
-server.listen(process.env.PORT || 3000);
+      socket.broadcast.emit('leave', socket.id)
+  })    
+})
+server.listen(process.env.PORT || 3000)
